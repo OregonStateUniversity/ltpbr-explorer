8253b415b326fc7742ab88b36acc73579f80c0ac
Nathan Struhs <nathanstruhs@gmail.com>
2018-01-10 10:47:23 -0800

    Fix controller test for root

--- test/controllers/static_controller_test.rb
+++ test/controllers/static_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StaticControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_home_url
+    get root_url
     assert_response :success
   end
 
