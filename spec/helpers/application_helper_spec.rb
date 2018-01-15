34ac0b2949f8799af4eaedab2452fc4435a8bead
Nathan Struhs <nathanstruhs@gmail.com>
2018-01-15 09:45:32 -0800

    Rubocop the project Just getting back into the groove, ignored rubocop documentation and schema

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe "#full_title" do
+  describe '#full_title' do
     it 'returns the base title without arguments' do
-      expect(helper.full_title).to eq('BDAdb');
+      expect(helper.full_title).to eq('BDAdb')
     end
 
     it 'returns the page title and screen title with page argument' do
