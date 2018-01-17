a33c9d0389025372801200d4e87bd1de5dedcd0d
Nathan Struhs <nathanstruhs@gmail.com>
2018-01-16 17:32:51 -0800

    Change login system to use username over email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,22 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  attr_accessor :login
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        conditions[:email].downcase! if conditions[:email]
+        where(username: conditions[:username]).first
+      end
+    end
+  end
 end
