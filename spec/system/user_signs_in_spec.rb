fd1c66c11515d1de07fb2e0005f52df9c4300bf5
Yong Bakos <ybakos@humanoriented.com>
2018-01-25 20:05:55 -0800

    Fix sign in button selector and align all semantics. References #6.

--- spec/system/user_signs_in_spec.rb
+++ spec/system/user_signs_in_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'user logs in' do
+RSpec.describe 'user signs in', :type => :feature do
 
-  def log_in_as(user)
+  def sign_in_as(user)
     visit new_user_session_path
     fill_in('Username', with: user.username)
     fill_in('Password', with: user.password)
-    click_button('Log in')
+    click_button('Sign In')
   end
 
   let(:user) { create(:user) }
 
-  it 'allows a valid user to log in' do
-    log_in_as(user)
+  it 'allows a valid user to sign in' do
+    sign_in_as(user)
     expect(page).to have_selector '.alert', text: 'Signed in successfully.'
   end
 end
