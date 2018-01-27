d3f29d947fc1e252b730695e3c29aa565d83c398
Yong Bakos <ybakos@humanoriented.com>
2018-01-26 18:26:50 -0800

    Remove affiliation from sign in form.

--- spec/system/user_edits_profile_spec.rb
+++ spec/system/user_edits_profile_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe 'User edits profile', :type => :feature do
   def sign_in_as(user)
     visit new_user_session_path
     fill_in('Username', with: user.username)
-    fill_in('Affiliation', with: user.affiliation)
     fill_in('Password', with: user.password)
     click_button('Sign In')
   end
