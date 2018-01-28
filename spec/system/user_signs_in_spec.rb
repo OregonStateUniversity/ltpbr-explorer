9468d0fa8b1637dbf97f3239cb2d3e66b2fa16f0
Yong Bakos <ybakos@humanoriented.com>
2018-01-28 08:35:21 -0800

    Test: Use rack driver for system test. References #6.

--- spec/system/user_signs_in_spec.rb
+++ spec/system/user_signs_in_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe 'User signs in', :type => :feature do
+RSpec.describe 'User signs in' do
 
-  def sign_in_as(user)
+  before do
+    driven_by :rack_test
     visit new_user_session_path
-    fill_in('Username', with: user.username)
-    fill_in('Password', with: user.password)
-    click_button('Sign In')
   end
 
   let(:user) { create(:user) }
 
   context 'with valid credentials' do
     it 'sees a success message' do
-      sign_in_as(user)
+      fill_in('Username', with: user.username)
+      fill_in('Password', with: user.password)
+      click_button('Sign In')
       expect(page).to have_selector '.notice', text: 'Signed in successfully.'
     end
   end
