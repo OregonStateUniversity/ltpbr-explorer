1a99bb4b7d1152f5e7fcef2eeb5d9f2d26021d39
Nathan Struhs <nathanstruhs@gmail.com>
2018-01-29 10:17:58 -0800

    Use production mailer on staging env for testing purposes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "beaver-dam-analogues_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https://bdadb.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'https://bdadb-staging.herokuapp.com/'}
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate
