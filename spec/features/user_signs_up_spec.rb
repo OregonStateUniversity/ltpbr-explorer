228bb57654f066a13b36af1e954ff01efb96710d
Nathan Struhs <nathanstruhs@gmail.com>
2018-01-30 15:26:02 -0800

    Fixes #13, implement user sign up feature test

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'User signs in', type: :feature do
+
+  before { visit new_user_registration_path }
+
+  context 'with valid signup information' do
+    it 'sees a success message' do
+      fill_in('Username', with: 'valid_username')
+      fill_in('Email', with: 'valid@email.com')
+      fill_in('Password', with: 'valid_password')
+      fill_in('Password confirmation', with: 'valid_password')
+      click_on('Sign up')
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+  end
+
+  context 'with invalid signup information' do
+    it 'sees a error message' do
+      fill_in('Username', with: '')
+      fill_in('Email', with: 'invalid@email')
+      fill_in('Password', with: 'a')
+      fill_in('Password confirmation', with: 'z')
+      click_on('Sign up')
+      expect(page).to have_selector '.alert-error'
+    end
+  end
+end
