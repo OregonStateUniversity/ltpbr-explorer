58fc61b274cfce2ade260f25fab85f36cf4ffb31
Nathan Struhs <nathanstruhs@gmail.com>
2018-01-31 10:11:46 -0800

    Ref #2, configure form errors for new projects

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def full_title(page_title = '')
     page_title.empty? ? BASE_TITLE : "#{page_title} | #{BASE_TITLE}"
   end
+
+  def form_errors_for(objects)
+    render partial: 'shared/form_errors', locals: { object: objects }
+  end
 end
