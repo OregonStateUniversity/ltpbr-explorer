7c76575255eab8ddf3a9ed8ad96857f1be57616c
Nathan Struhs <nathanstruhs@gmail.com>
2018-02-09 12:07:27 -0800

    Ref #23, redirect to user#show after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    current_user
+  end
 end
