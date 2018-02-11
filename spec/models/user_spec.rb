12eeecc8bc615359539556aaee6bed835b74a7fd
Yong Bakos <ybakos@humanoriented.com>
2018-02-10 21:23:33 -0800

    User: Test projects association.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject(:user) { build(:user) }
 
-  describe 'validations' do
+  describe 'validations and associations' do
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_uniqueness_of(:username).ignoring_case_sensitivity }
+    it { is_expected.to have_many(:projects) }
   end
 end
