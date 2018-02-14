af3cb1d50ca5bcd0a3f6cc3b9abbd1065fe2b38d
Nathan Struhs <nathanstruhs@gmail.com>
2018-02-14 10:29:25 -0800

    Spec: test project validations

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -2,4 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Project, type: :model do
   subject(:project) { build(:project) }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:affiliation) }
+    it { is_expected.to validate_presence_of(:stream_name) }
+    it { is_expected.to validate_presence_of(:implementation_date) }
+    it { is_expected.to validate_presence_of(:primary_contact) }
+    it { is_expected.to validate_presence_of(:longitude) }
+    it { is_expected.to validate_presence_of(:latitude) }
+    it { is_expected.to validate_numericality_of(:area).only_integer.is_greater_than(0) }
+  end
+
 end
