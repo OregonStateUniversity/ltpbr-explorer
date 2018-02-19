ac14b54fd3573c727aa07bfc1467c3be347f1480
Nathan Struhs <nathanstruhs@gmail.com>
2018-02-19 12:50:40 -0800

    Remove ES6 style const variables

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -2,8 +2,8 @@ $(".projects.show").ready(function() {
   var map_id = $('#map');
   var latitude = map_id.data('latitude');
   var longitude = map_id.data('longitude');
-  const zoom = 15;
-  const max_zoom = 17;
+  var zoom = 15;
+  var max_zoom = 17;
   var leaflet_map = L.map("map").setView([latitude, longitude], zoom);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3RydWhzbiIsImEiOiJjamRydHJva3YyZ3huMnhvNDNxYTQ0bzg0In0.2tYsJnemg4TMn-1GnAmc1Q', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
