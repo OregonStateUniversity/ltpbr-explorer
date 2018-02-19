33a64dddd66debe67c057fbf52587e44c1a3c565
Nathan Struhs <nathanstruhs@gmail.com>
2018-02-19 13:23:41 -0800

    Include lat/lon validations in model spec

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -11,6 +11,8 @@ RSpec.describe Project, type: :model do
     it { is_expected.to validate_presence_of(:longitude) }
     it { is_expected.to validate_presence_of(:latitude) }
     it { is_expected.to validate_numericality_of(:area).only_integer.is_greater_than(0) }
+    it { is_expected.to validate_numericality_of(:latitude).is_greater_than(-90).is_less_than(90) }
+    it { is_expected.to validate_numericality_of(:longitude).is_greater_than(-180).is_less_than(180) }
   end
 
   describe 'associations' do
