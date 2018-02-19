580af0145c177220588c5a164036f29d5c43c754
Nathan Struhs <nathanstruhs@gmail.com>
2018-02-19 12:49:00 -0800

    Fixes #29, style project#show screen Rename maps.js variables

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,13 +1,15 @@
 $(".projects.show").ready(function() {
-  var map = $('#map');
-  var latitude = map.data('latitude');
-  var longitude = map.data('longitude');
-  var mymap = L.map("map").setView([latitude, longitude], 15);
+  var map_id = $('#map');
+  var latitude = map_id.data('latitude');
+  var longitude = map_id.data('longitude');
+  const zoom = 15;
+  const max_zoom = 17;
+  var leaflet_map = L.map("map").setView([latitude, longitude], zoom);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3RydWhzbiIsImEiOiJjamRydHJva3YyZ3huMnhvNDNxYTQ0bzg0In0.2tYsJnemg4TMn-1GnAmc1Q', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 17,
+    maxZoom: max_zoom,
     id: 'mapbox.outdoors',
-    accessToken: 'your.mapbox.access.token'
-  }).addTo(mymap);
-  var marker = L.marker([latitude, longitude]).addTo(mymap);
+    accessToken: 'pk.eyJ1Ijoic3RydWhzbiIsImEiOiJjamRydHJva3YyZ3huMnhvNDNxYTQ0bzg0In0.2tYsJnemg4TMn-1GnAmc1Q'
+  }).addTo(leaflet_map);
+  var marker = L.marker([latitude, longitude]).addTo(leaflet_map);
 });
