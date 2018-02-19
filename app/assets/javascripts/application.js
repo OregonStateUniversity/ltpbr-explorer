549bf6fad907e7fdb25312685635cb49e612d282
Nathan Struhs <nathanstruhs@gmail.com>
2018-02-19 12:20:49 -0800

    Ref #29, populate map with dynamic lat/long Use jquery-readyselector.js to only load js on certain pages Assign data values in html and grab them in js to populate map with coords

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,6 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery-readyselector
 //= require maps
 //= require_tree .
--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-  var mymap = L.map("map").setView([44.063707, -121.337813], 15);
+$(".projects.show").ready(function() {
+  var map = $('#map');
+  var latitude = map.data('latitude');
+  var longitude = map.data('longitude');
+  var mymap = L.map("map").setView([latitude, longitude], 15);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3RydWhzbiIsImEiOiJjamRydHJva3YyZ3huMnhvNDNxYTQ0bzg0In0.2tYsJnemg4TMn-1GnAmc1Q', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.outdoors',
     accessToken: 'your.mapbox.access.token'
   }).addTo(mymap);
-  var marker = L.marker([44.063707, -121.337813]).addTo(mymap);
-  marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+  var marker = L.marker([latitude, longitude]).addTo(mymap);
 });
