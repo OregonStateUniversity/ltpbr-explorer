19d880d5cf86a3a6f78922fe98a4b2e3431132be
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-02 11:14:32 -0800

    Add turbolinks:load event to scroll functions

--- app/assets/javascripts/master-map-scroll.js
+++ app/assets/javascripts/master-map-scroll.js
@@ -1,13 +1,15 @@
-$(".static.home").ready(function() {
-  $('.btn-view-map').click(function() {
-    $('html, body').animate({
-      scrollTop: $('#master-map').offset().top
-      }, 1000);
-  });
+$(document).on('turbolinks:load', function() {
+  $(".static.home").ready(function() {
+    $('.btn-view-map').click(function() {
+      $('html, body').animate({
+        scrollTop: $('#master-map').offset().top
+        }, 1000);
+    });
 
-  $('.btn-map-go-back').click(function() {
-    $('html, body').animate({
-      scrollTop: 0
-      }, 1000);
+    $('.btn-map-go-back').click(function() {
+      $('html, body').animate({
+        scrollTop: 0
+        }, 1000);
+    });
   });
 });
