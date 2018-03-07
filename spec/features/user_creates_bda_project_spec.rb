53804bf668548df6ba3d5d55fce815cf63bda0ef
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-06 16:57:48 -0800

    Update feature spec to reflect change in form checkbox

--- spec/features/user_creates_bda_project_spec.rb
+++ spec/features/user_creates_bda_project_spec.rb
@@ -19,7 +19,7 @@ RSpec.feature 'User creates bda project' do
       fill_in('Longitude', with: 45.0000)
       fill_in('Narrative', with: 'Example Project Narrative')
       fill_in('Treatment length', with: 1000)
-      check('Maintenance?')
+      check('Will this project requires adaptive management?')
       fill_in('Primary contact', with: 'Example Contact Name')
       click_on('Create Project')
       expect(page).to have_content('Project was successfully created.')
