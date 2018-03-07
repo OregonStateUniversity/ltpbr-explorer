c038bae10397f2d8ab014b25a8344d0cac32a037
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-07 10:23:15 -0800

    Fixed #36, Hardcode map center to show all of oregon

--- app/assets/javascripts/master-map.js
+++ app/assets/javascripts/master-map.js
@@ -4,9 +4,12 @@ $(document).on('turbolinks:load', function() {
     var map_id = $('#master-map');
     var latitude = map_id.data('latitude');
     var longitude = map_id.data('longitude');
-    var zoom = 10;
+
+    var zoom = 7;
     var max_zoom = 17;
-    var leaflet_map = L.map("master-map").setView([projects[0].latitude, projects[0].longitude], zoom);
+    var oregon_center_lat = 44.053592;
+    var oregon_center_long = -120.379945;
+    var leaflet_map = L.map("master-map").setView([oregon_center_lat, oregon_center_long], zoom);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3RydWhzbiIsImEiOiJjamRydHJva3YyZ3huMnhvNDNxYTQ0bzg0In0.2tYsJnemg4TMn-1GnAmc1Q', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
