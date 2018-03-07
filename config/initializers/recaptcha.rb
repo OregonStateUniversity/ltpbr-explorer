d75c52ff2ca8b35b2a76b46487309d0fc971642b
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-07 12:07:05 -0800

    Capitalize env keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = ENV['recaptcha_site_key']
-  config.secret_key = ENV['recaptcha_secret_key']
+  config.site_key  = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
