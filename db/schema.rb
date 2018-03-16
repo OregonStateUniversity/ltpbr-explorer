127e2bdbb4c6dbae7af20e116138c809d7ea2365
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-15 17:32:32 -0700

    Fixed #49, Fixed #50, add not null constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180219190607) do
+ActiveRecord::Schema.define(version: 20180316003002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
