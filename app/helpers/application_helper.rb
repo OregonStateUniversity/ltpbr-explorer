3400c8cfba2d1c6cb5adb11157eb654565a31b4b
Nathan Struhs <nathanstruhs@gmail.com>
2018-03-30 14:02:05 -0700

    Update base title to reflect new application name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  BASE_TITLE = 'BDAdb: Beaver Dam Analogues'.freeze
+  BASE_TITLE = 'Beaver Dam Analog Explorer'.freeze
   def full_title(page_title = '')
     page_title.empty? ? BASE_TITLE : "#{page_title} | #{BASE_TITLE}"
   end
