49b44755bd20973aec7e40aaa465b80725d0f59e
Yong Bakos <ybakos@humanoriented.com>
2018-04-10 07:48:46 -0700

    Spelling: Analog -> Analogue. Per the research literature.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe '#full_title' do
     it 'returns the base title without arguments' do
-      expect(helper.full_title).to eq('Beaver Dam Analog Explorer')
+      expect(helper.full_title).to eq('Beaver Dam Analogue Explorer')
     end
 
     it 'returns the page title and screen title with page argument' do
-      expect(helper.full_title('Home')).to eq('Home | Beaver Dam Analog Explorer')
+      expect(helper.full_title('Home')).to eq('Home | Beaver Dam Analogue Explorer')
     end
   end
 end
