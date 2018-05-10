d0fa98a44bf0744dd02b75a5cf62df9d1980e7e7
Nathan Struhs <nathanstruhs@gmail.com>
2018-05-10 12:59:50 -0700

    Fixed #52, Fixed #45, Fixed #42, configure project form guidance

--- app/assets/javascripts/form-modal.js
+++ app/assets/javascripts/form-modal.js
@@ -1,5 +1,29 @@
 $(document).on('turbolinks:load', function() {
+  $('#affiliation-help').on('click', function() {
+    $('#affiliation-model').modal();
+  });
+
+  $('#implementation-date-help').on('click', function() {
+    $('#implementation-date-model').modal();
+  });
+
+  $('#latitude-help').on('click', function() {
+    $('#latitude-model').modal();
+  });
+
+  $('#longitude-help').on('click', function() {
+    $('#longitude-model').modal();
+  });
+
+  $('#treatment-length-help').on('click', function() {
+    $('#treatment-length-model').modal();
+  });
+
   $('#narrative-help').on('click', function() {
     $('#narrative-model').modal();
+  });
+
+  $('#primary-contact-help').on('click', function() {
+    $('#primary-contact-model').modal();
   });
 });
