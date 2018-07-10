bfcb0b80b9e7abbbe210db91f2b9ff9088c6d2ad
Yong Bakos <ybakos@humanoriented.com>
2018-07-10 00:47:54 -0700

    ProjectsController: Remove double-assignment of association.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,8 +18,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    @project.author_id = current_user.id
-    current_user.projects << @project
+    @project.author = current_user
     if @project.save
       redirect_to @project
       flash[:success] = 'Project was successfully created.'
