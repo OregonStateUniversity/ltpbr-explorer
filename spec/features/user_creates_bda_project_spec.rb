db8d22220cf9bfc7ffb991bca9cd3881b985fca1
Yong Bakos <ybakos@humanoriented.com>
2018-07-21 22:50:29 -0700

    user_creates_bda_project_spec: Remove outer logged in context. Just assume it.

--- spec/features/user_creates_bda_project_spec.rb
+++ spec/features/user_creates_bda_project_spec.rb
@@ -1,45 +1,43 @@
 require 'rails_helper'
 
-RSpec.feature 'User creates bda project' do
+RSpec.feature 'User creates BDA project' do
   include Devise::Test::IntegrationHelpers
 
-  context 'when logged in' do
-    let(:user) { create(:user) }
+  let(:user) { create(:user) }
 
-    before do
-      sign_in(user)
-      visit new_project_path
-    end
+  before do
+    sign_in(user)
+    visit new_project_path
+  end
 
-    scenario 'providing valid project attributes' do
-      fill_in('Affiliation', with: 'Example Affiliation')
-      fill_in('Stream name', with: 'Example Stream Name')
-      fill_in('Implementation date', with: Date.today)
-      fill_in('Latitude', with: 45.0000)
-      fill_in('Longitude', with: 45.0000)
-      fill_in('Narrative', with: 'Example Project Narrative')
-      fill_in('Treatment length', with: 1000)
-      check('Will this project requires adaptive management?')
-      fill_in('Primary contact', with: 'Example Contact Name')
-      click_on('Create Project')
-      expect(page).to have_content('Project was successfully created.')
-    end
+  scenario 'providing valid project attributes' do
+    fill_in('Affiliation', with: 'Example Affiliation')
+    fill_in('Stream name', with: 'Example Stream Name')
+    fill_in('Implementation date', with: Date.today)
+    fill_in('Latitude', with: 45.0000)
+    fill_in('Longitude', with: 45.0000)
+    fill_in('Narrative', with: 'Example Project Narrative')
+    fill_in('Treatment length', with: 1000)
+    check('Will this project requires adaptive management?')
+    fill_in('Primary contact', with: 'Example Contact Name')
+    click_on('Create Project')
+    expect(page).to have_content('Project was successfully created.')
+  end
 
-    scenario 'providing invalid project attributes' do
-      click_on('Create Project')
-      expect(page).to have_selector '.alert', text: 'The form contains 10 errors.'
-      page.find('#error_explanation').tap do |error_explanations|
-        expect(error_explanations).to have_content("Affiliation can't be blank")
-        expect(error_explanations).to have_content("Stream name can't be blank")
-        expect(error_explanations).to have_content("Implementation date can't be blank")
-        expect(error_explanations).to have_content("Implementation date must be in the following format: yyyy-mm-dd")
-        expect(error_explanations).to have_content("Latitude can't be blank")
-        expect(error_explanations).to have_content('Latitude must be in decimal notation')
-        expect(error_explanations).to have_content("Longitude can't be blank")
-        expect(error_explanations).to have_content('Longitude must be in decimal notation')
-        expect(error_explanations).to have_content("Treatment length is not a number")
-        expect(error_explanations).to have_content("Primary contact can't be blank")
-      end
+  scenario 'providing invalid project attributes' do
+    click_on('Create Project')
+    expect(page).to have_selector '.alert', text: 'The form contains 10 errors.'
+    page.find('#error_explanation').tap do |error_explanations|
+      expect(error_explanations).to have_content("Affiliation can't be blank")
+      expect(error_explanations).to have_content("Stream name can't be blank")
+      expect(error_explanations).to have_content("Implementation date can't be blank")
+      expect(error_explanations).to have_content("Implementation date must be in the following format: yyyy-mm-dd")
+      expect(error_explanations).to have_content("Latitude can't be blank")
+      expect(error_explanations).to have_content('Latitude must be in decimal notation')
+      expect(error_explanations).to have_content("Longitude can't be blank")
+      expect(error_explanations).to have_content('Longitude must be in decimal notation')
+      expect(error_explanations).to have_content("Treatment length is not a number")
+      expect(error_explanations).to have_content("Primary contact can't be blank")
     end
   end
 end
