6d8a48d1beba50b5063f8a2d99ef4d4309debc1f
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-10-04 22:05:21 -0700

    Project: Fix form labels and update test.
    @webwenick79 Use the f.label approach. The labels didn't match up w/ the field names, so the
    tests broke (which is good).

--- spec/features/user_creates_bda_project_spec.rb
+++ spec/features/user_creates_bda_project_spec.rb
@@ -11,16 +11,18 @@ RSpec.feature 'User creates BDA project' do
   end
 
   scenario 'providing valid project attributes' do
-    fill_in('Affiliation', with: 'Example Affiliation')
-    fill_in('Stream name', with: 'Example Stream Name')
-    fill_in('Implementation date', with: Date.today)
+    fill_in('Project Name', with: 'Example Name')
+    fill_in('Primary Contact Name', with: 'Example Contact Name')
+    fill_in('Project Affiliation', with: 'Example Affiliation')
+    fill_in('Stream Name', with: 'Example Stream Name')
+    fill_in('Project Watershed', with: 'Example Watershed')
     fill_in('Latitude', with: 45.0000)
     fill_in('Longitude', with: 45.0000)
-    fill_in('Number of structures', with: 2)
-    fill_in('Narrative', with: 'Example Project Narrative')
-    fill_in('Structure Design Elements', with: 'Example structure design elements')
-    fill_in('Treatment length', with: 1000)
-    fill_in('Primary contact', with: 'Example Contact Name')
+    fill_in('Total BDA Structures', with: 2)
+    fill_in('Treatment Length (meters)', with: 1000)
+    fill_in('Implementation Date', with: Date.today)
+    fill_in('Project Restoration Goals', with: 'Example Project Narrative')
+    fill_in('Structure Construction Elements', with: 'Example structure design elements')
     click_on('Create Project')
     expect(page).to have_content('Project was successfully created.')
   end
