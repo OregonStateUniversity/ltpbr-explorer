2e1e9353aef4bd0982de43531334d574a19196c0
Yong Joseph Bakos <ybakos@humanoriented.com>
2018-10-25 21:54:32 -0700

    Config: Move airbrake config to production config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,3 +116,15 @@ Rails.application.configure do
     }
   }
 end
+
+# https://github.com/airbrake/airbrake-ruby#configuration
+Airbrake.configure do |c|
+  c.project_id = ENV['AIRBRAKE_PROJECT_ID']
+  c.project_key = ENV['AIRBRAKE_API_KEY']
+  c.root_directory = Rails.root
+  c.logger = Rails.logger
+  c.environment = Rails.env
+  c.ignore_environments = %w(test)
+  c.blacklist_keys = [/password/i, /authorization/i]
+end
+
