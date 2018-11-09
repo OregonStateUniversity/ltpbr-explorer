74a6ca8653dcd6a066418fe86f3a6a5d819100c6
Nick Weber <webernick@icloud.com>
2018-11-09 12:36:53 -0800

    formatting of show page

--- db/schema.rb
+++ db/schema.rb
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2018_10_27_045244) do
     t.datetime "updated_at", null: false
     t.string "username", null: false
     t.string "affiliation", null: false
-    t.string "name", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
