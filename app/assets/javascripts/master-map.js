7698a279b81e9e6808f47f3fda47d15854382140
Nick Weber <webernick@icloud.com>
2020-05-15 14:12:23 -0700

    Adds map features

--- app/assets/javascripts/master-map.js
+++ app/assets/javascripts/master-map.js
@@ -1,35 +1,60 @@
-$(document).on('turbolinks:load', function() {
-  $(".static.projects_map").ready(function() {
+$(document).on('turbolinks:load', function () {
+  $(".static.projects_map").ready(function () {
     var projects = gon.projects.projects;
-    var map_id = $('#master-map');
-    var latitude = map_id.data('latitude');
-    var longitude = map_id.data('longitude');
+    var zoom = 6;
+    var map_center_lat = 44.5;
+    var map_center_lon = -120.5;
 
-    var zoom = 7;
-    var max_zoom = 17;
-    var oregon_center_lat = 44.053592;
-    var oregon_center_long = -120.379945;
-    var leaflet_map = L.map("master-map").setView([oregon_center_lat, oregon_center_long], zoom);
+    var Esri_NatGeoWorldMap = L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}', {
+        minZoom: 1,
+        maxZoom: 16
+      }
+    );
 
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
-  attribution: 'Tiles &copy; Esri &mdash; and the GIS User Community',
-      maxZoom: max_zoom,
-      id: 'Esri.WorldTopoMap',
-      // accessToken: 'pk.eyJ1IjoieWJha29zIiwiYSI6ImNqamZmbGh4aTA2MWszcXJtM3phbWlyenoifQ.q9CV0qTFVrReLsKnrj5ALg'
-    }).addTo(leaflet_map);
+    var Esri_WorldImagery = L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}');
 
-    for (var i = 0; i < projects.length; i++ ) {
-      var marker = L.marker([projects[i].latitude, projects[i].longitude]).addTo(leaflet_map);
-      marker.bindPopup("<b>"
-                        + projects[i].name
-                        + "</b><br>Stream: "
-                        + projects[i].stream_name
-                        + "</b><br>Watershed: "
-                        + projects[i].watershed
-                        + "<br><a href='/projects/"
-                        + projects[i].id
-                        + "'>View Project</a>"
-                      );
+    var baseLayers = {
+      'Topographic': Esri_NatGeoWorldMap,
+      'Satellite Imagery': Esri_WorldImagery
+    };
+
+    // Create the map
+    var project_map = L.map("master-map").setView([map_center_lat, map_center_lon], zoom);
+
+    // Add imagery
+    Esri_NatGeoWorldMap.addTo(project_map);
+
+
+    // layer control
+    var layer_control = L.control.layers(baseLayers).addTo(project_map);
+
+    // add scale bar
+    L.control.scale().addTo(project_map);
+
+    // project points now
+    var Projects = L.featureGroup();
+
+    for (var i = 0; i < projects.length; i++) {
+      var marker = L.marker([projects[i].latitude, projects[i].longitude]);
+      marker.bindPopup("<b>" +
+        projects[i].name +
+        "</b><br>Stream: " +
+        projects[i].stream_name +
+        "</b><br>Watershed: " +
+        projects[i].watershed +
+        "<br><a href='/projects/" +
+        projects[i].id +
+        "'>View Project</a>"
+      );
+      Projects.addLayer(marker);
     }
+
+    // add all projects to map
+    Projects.addTo(project_map);
+
+    // fit display to points on the map
+    project_map.fitBounds(Projects.getBounds());
   });
 });
