c6a784518840fa42ea4ee0d1f8fc9751573e888f
Yong Bakos <ybakos@humanoriented.com>
2021-04-16 20:57:01 -0700

    Config: Update smtp config for mailgun.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,13 +67,12 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://bda-explorer.herokuapp.com/'}
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address:              'smtp.sendgrid.net',
-    port:                 587,
-    domain:               'heroku.com',
-    user_name:            ENV["SENDGRID_USERNAME"],
-    password:             ENV["SENDGRID_PASSWORD"],
-    authentication:       'plain',
-    enable_starttls_auto: true
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    port:                 ENV['MAILGUN_SMTP_PORT'],
+    domain:               'bda-explorer.herokuapp.com',
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
+    authentication:       :plain,
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
