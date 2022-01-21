f5e0830e1c4c46780295739626ece4a064f78b70
nueadkie <chris.j.miraflor@gmail.com>
2022-01-21 00:09:45 -0800

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_045536) do
+ActiveRecord::Schema.define(version: 2022_01_21_053859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2022_01_14_045536) do
   end
 
   create_table "affiliations", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
+    t.string "name", null: false
+    t.text "description"
     t.string "icon_name"
     t.integer "icon_file_size"
     t.string "icon_content_type"
@@ -69,25 +69,8 @@ ActiveRecord::Schema.define(version: 2022_01_14_045536) do
     t.text "url"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet "current_sign_in_ip"
-    t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "username", null: false
-    t.string "affiliation", null: false
-    t.string "name", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'user_role' for column 'role'
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "projects", "users", column: "author_id"
