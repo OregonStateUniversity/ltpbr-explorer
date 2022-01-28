0378ace58ed8ee4940c71b8a901908ae1230ef38
nueadkie <chris.j.miraflor@gmail.com>
2022-01-27 23:40:22 -0800

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_21_053859) do
+ActiveRecord::Schema.define(version: 2022_01_23_002225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
+
+  create_enum :user_role, [
+    "public",
+    "admin",
+  ], force: :cascade
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -37,16 +42,6 @@ ActiveRecord::Schema.define(version: 2022_01_21_053859) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "affiliations", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "icon_name"
-    t.integer "icon_file_size"
-    t.string "icon_content_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "projects", force: :cascade do |t|
     t.string "affiliation", null: false
     t.string "stream_name", null: false
@@ -58,19 +53,37 @@ ActiveRecord::Schema.define(version: 2022_01_21_053859) do
     t.datetime "updated_at", null: false
     t.geography "lonlat", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}, null: false
     t.integer "author_id", null: false
-    t.string "photo_file_name"
-    t.string "photo_content_type"
-    t.integer "photo_file_size"
-    t.datetime "photo_updated_at"
     t.integer "number_of_structures", null: false
     t.text "structure_description", null: false
     t.string "name", null: false
     t.string "watershed", null: false
     t.text "url"
+    t.string "photo_file_name"
+    t.string "photo_content_type"
+    t.integer "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'user_role' for column 'role'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "username", null: false
+    t.string "affiliation", null: false
+    t.string "name", null: false
+    t.enum "role", default: "public", enum_type: "user_role"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "projects", "users", column: "author_id"
