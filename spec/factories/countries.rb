71bfd0573eacc2046c986a67948527e02d667e68
nueadkie <chris.j.miraflor@gmail.com>
2022-02-18 11:08:15 -0800

    Added model for countries

---
+++ spec/factories/countries.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :country do
+    name { "MyString" }
+    iso_code { "MyString" }
+    geom { "" }
+    total_length { 1 }
+    total_number_of_structures { 1 }
+  end
+end
