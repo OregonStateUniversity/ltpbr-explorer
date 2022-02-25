2a819e348ec12b96f16e273a2b9d6d0aac1b37b4
nueadkie <chris.j.miraflor@gmail.com>
2022-02-25 12:56:11 -0800

    Added pages for showing individual states

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,4 +3,10 @@ class StatesController < ApplicationController
   def index
     @states = State.select(*State.attribute_names.reject{ |attr| attr=='geom' }).order(:name)
   end
+
+  def show
+    @state = State.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to states_path, warning: 'That state does not exist.'
+  end
 end
