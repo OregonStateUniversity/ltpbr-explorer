1d0b34c09a56374f98eecbf8db05155ccbaa8c24
nueadkie <chris.j.miraflor@gmail.com>
2022-03-04 09:22:19 -0800

    Removed extraneous SQL select

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -1,5 +1,4 @@
 class StatesController < ApplicationController
-  @states = State.select(*State.attribute_names.reject{ |attr| attr=='geom' })
   def index
     @states = State.select(*State.attribute_names.reject{ |attr| attr=='geom' }).order(:name)
   end
