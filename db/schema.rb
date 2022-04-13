91f744bc3918a8849ffa2883622cafaa0177f8f5
Chris Miraflor <mirafloc@oregonstate.edu>
2022-04-13 14:35:55 -0700

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_202502) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_02_18_202502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -26,7 +25,7 @@ ActiveRecord::Schema.define(version: 2022_02_18_202502) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -38,13 +37,13 @@ ActiveRecord::Schema.define(version: 2022_02_18_202502) do
     t.text "metadata"
     t.bigint "byte_size", null: false
     t.string "checksum"
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: nil, null: false
     t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
+    t.integer "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -53,8 +52,8 @@ ActiveRecord::Schema.define(version: 2022_02_18_202502) do
     t.bigint "project_id", null: false
     t.bigint "organization_id", null: false
     t.string "role"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["organization_id"], name: "index_affiliations_on_organization_id"
     t.index ["project_id"], name: "index_affiliations_on_project_id"
   end
@@ -64,8 +63,8 @@ ActiveRecord::Schema.define(version: 2022_02_18_202502) do
     t.text "description"
     t.string "contact"
     t.string "website"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "projects", force: :cascade do |t|
@@ -75,14 +74,14 @@ ActiveRecord::Schema.define(version: 2022_02_18_202502) do
     t.text "narrative", null: false
     t.integer "length", null: false
     t.string "primary_contact", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.geography "lonlat", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}, null: false
     t.integer "author_id", null: false
     t.string "photo_file_name"
     t.string "photo_content_type"
     t.integer "photo_file_size"
-    t.datetime "photo_updated_at"
+    t.datetime "photo_updated_at", precision: nil
     t.integer "number_of_structures", null: false
     t.text "structure_description", null: false
     t.string "name", null: false
@@ -97,8 +96,8 @@ ActiveRecord::Schema.define(version: 2022_02_18_202502) do
     t.string "iso_code"
     t.string "state_type"
     t.geometry "geom", limit: {:srid=>0, :type=>"multi_polygon"}
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["geom"], name: "index_states_on_geom", using: :gist
   end
 
@@ -106,15 +105,15 @@ ActiveRecord::Schema.define(version: 2022_02_18_202502) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.string "username", null: false
     t.string "affiliation", null: false
     t.string "name", null: false
@@ -124,6 +123,7 @@ ActiveRecord::Schema.define(version: 2022_02_18_202502) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "affiliations", "organizations"
   add_foreign_key "affiliations", "projects"
   add_foreign_key "projects", "states"
