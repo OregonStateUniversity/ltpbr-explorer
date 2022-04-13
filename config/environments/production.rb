a926407cb2387d03f31707c2c7c6314ca6a69ba9
Chris Miraflor <mirafloc@oregonstate.edu>
2022-04-13 16:33:22 -0700

    Readded Airbrake to production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,4 +101,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  
+end
+
+# https://github.com/airbrake/airbrake-ruby#configuration
+Airbrake.configure do |c|
+  c.project_id = ENV['AIRBRAKE_PROJECT_ID']
+  c.project_key = ENV['AIRBRAKE_API_KEY']
+  c.root_directory = Rails.root
+  c.logger = Rails.logger
+  c.environment = Rails.env
+  c.ignore_environments = %w(test)
+  c.blocklist_keys = [/password/i, /authorization/i]
 end
