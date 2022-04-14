0c50e8c399af17c3f90087d36138d40b54fc6d6e
Chris Miraflor <mirafloc@oregonstate.edu>
2022-04-13 17:19:11 -0700

    Changed config comment to clarify the use of AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+  # Store uploaded files on an Amazon S3 bucket (see config/storage.yml for options).
   config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
