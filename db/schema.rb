56149417ca44a6d63a08c053fb2446b0d677fb28
Chris Miraflor <mirafloc@oregonstate.edu>
2022-04-13 21:31:37 -0700

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_18_202502) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_14_010437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
@@ -78,10 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_18_202502) do
     t.datetime "updated_at", precision: nil, null: false
     t.geography "lonlat", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}, null: false
     t.integer "author_id", null: false
-    t.string "photo_file_name"
-    t.string "photo_content_type"
-    t.integer "photo_file_size"
-    t.datetime "photo_updated_at", precision: nil
     t.integer "number_of_structures", null: false
     t.text "structure_description", null: false
     t.string "name", null: false
