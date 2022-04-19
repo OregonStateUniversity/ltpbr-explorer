0b432e5d5addc4b0fdb639b3e29c8c6e0814609a
cooperphyz <gjcooper99@gmail.com>
2022-04-19 10:10:17 -0700

    Add permissions for CRUD exclusive to admin

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,7 +1,7 @@
 class OrganizationsController < ApplicationController
   before_action :set_organization, only: %w[ show edit update destroy ]
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :require_admin, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:edit, :show, :create, :update, :destroy]
 
   # GET /organizations
   # GET /organizations.json
