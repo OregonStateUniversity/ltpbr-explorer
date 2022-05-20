1df52070def95adfbd904866e0d109a79181cb00
Chris Miraflor <mirafloc@oregonstate.edu>
2022-05-20 01:03:42 -0700

    Fix tests (#271)
    * Replaced older placeholder test coordinates with coordinates for OSU-Cascades
    
    * Updated config to remove deprecation message from tests
    
    * Removed old affiliation code from project creation feature specs
    
    * Added checks for project URL to specs
    
    * Added expected for validating presence for project URL
    
    * Removed unused byline method from project model
    
    * Removed check for validating presence of old affiliation
    
    * Fixed lonlat generation tests
    
    * Added factory for admin users
    
    * Added factory for example organization
    
    * Commented out request spec for old affiliation scaffold
    
    * Updated URLs for affiliations routing spec
    
    * Fixed stub model description truncation
    
    * Added name & watershed to projects factory
    
    * Added stub models to organizations view spec
    
    * Removed view specs for old affiliations pages
    
    * Removed unused factory
    
    * Added valid organization hash
    
    * Removed tests for invalid parameters
    
    * Added test for updating organization attributes
    
    * Remove unused specs

--- spec/requests/organizations_spec.rb
+++ spec/requests/organizations_spec.rb
@@ -13,11 +13,18 @@
 # sticking to rails and rspec-rails APIs to keep things simple and stable.
 
 RSpec.describe "/organizations", type: :request do
+  include Devise::Test::IntegrationHelpers
+
+  let(:user) { create(:user, :admin) }
+
+  before do
+    sign_in(user)
+  end
   
   # Organization. As you add validations to Organization, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    attributes_for(:organization)
   }
 
   let(:invalid_attributes) {
@@ -69,31 +76,39 @@ RSpec.describe "/organizations", type: :request do
       end
     end
 
-    context "with invalid parameters" do
-      it "does not create a new Organization" do
-        expect {
-          post organizations_url, params: { organization: invalid_attributes }
-        }.to change(Organization, :count).by(0)
-      end
+    # context "with invalid parameters" do
+    #   it "does not create a new Organization" do
+    #     expect {
+    #       post organizations_url, params: { organization: invalid_attributes }
+    #     }.to change(Organization, :count).by(0)
+    #   end
 
-      it "renders a successful response (i.e. to display the 'new' template)" do
-        post organizations_url, params: { organization: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
+    #   it "renders a successful response (i.e. to display the 'new' template)" do
+    #     post organizations_url, params: { organization: invalid_attributes }
+    #     expect(response).to be_successful
+    #   end
+    # end
   end
 
   describe "PATCH /update" do
     context "with valid parameters" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        {
+          name: "New Organization Name",
+          description: "New organization description",
+          contact: "New organization contact",
+          website: "example.com/neworganization"
+        }
       }
 
       it "updates the requested organization" do
         organization = Organization.create! valid_attributes
         patch organization_url(organization), params: { organization: new_attributes }
         organization.reload
-        skip("Add assertions for updated state")
+        expect(organization.name).to eq "New Organization Name"
+        expect(organization.description).to eq "New organization description"
+        expect(organization.contact).to eq "New organization contact"
+        expect(organization.website).to eq "example.com/neworganization"
       end
 
       it "redirects to the organization" do
@@ -104,13 +119,13 @@ RSpec.describe "/organizations", type: :request do
       end
     end
 
-    context "with invalid parameters" do
-      it "renders a successful response (i.e. to display the 'edit' template)" do
-        organization = Organization.create! valid_attributes
-        patch organization_url(organization), params: { organization: invalid_attributes }
-        expect(response).to be_successful
-      end
-    end
+    # context "with invalid parameters" do
+    #   it "renders a successful response (i.e. to display the 'edit' template)" do
+    #     organization = Organization.create! valid_attributes
+    #     patch organization_url(organization), params: { organization: invalid_attributes }
+    #     expect(response).to be_successful
+    #   end
+    # end
   end
 
   describe "DELETE /destroy" do
