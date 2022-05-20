9e16e2d16ed8fce50c8a53dc383507949c5b6827
Chris Miraflor <mirafloc@oregonstate.edu>
2022-05-19 22:52:50 -0700

    Added factory for example organization

--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :organization do
-    
+    name { "Example Organization" }
+    description { "Example organization description"}
+    contact { "Example organization contact info" }
+    website { "example.com/organization" }
   end
 end
