66ad5787f54a371fff576e936746d0fec813cfbc
Chris Miraflor <mirafloc@oregonstate.edu>
2022-05-19 23:45:13 -0700

    Updated URLs for affiliations routing spec

--- spec/routing/affiliations_routing_spec.rb
+++ spec/routing/affiliations_routing_spec.rb
@@ -3,36 +3,36 @@ require "rails_helper"
 RSpec.describe AffiliationsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(get: "/affiliations").to route_to("affiliations#index")
+      expect(get: "/projects/1/affiliations").to route_to("affiliations#index", project_id: "1")
     end
 
     it "routes to #new" do
-      expect(get: "/affiliations/new").to route_to("affiliations#new")
+      expect(get: "/projects/1/affiliations/new").to route_to("affiliations#new", project_id: "1")
     end
 
     it "routes to #show" do
-      expect(get: "/affiliations/1").to route_to("affiliations#show", id: "1")
+      expect(get: "/projects/1/affiliations/1").to route_to("affiliations#show", id: "1", project_id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/affiliations/1/edit").to route_to("affiliations#edit", id: "1")
+      expect(get: "/projects/1/affiliations/1/edit").to route_to("affiliations#edit", id: "1", project_id: "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/affiliations").to route_to("affiliations#create")
+      expect(post: "/projects/1/affiliations").to route_to("affiliations#create", project_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/affiliations/1").to route_to("affiliations#update", id: "1")
+      expect(put: "/projects/1/affiliations/1").to route_to("affiliations#update", id: "1", project_id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/affiliations/1").to route_to("affiliations#update", id: "1")
+      expect(patch: "/projects/1/affiliations/1").to route_to("affiliations#update", id: "1", project_id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/affiliations/1").to route_to("affiliations#destroy", id: "1")
+      expect(delete: "/projects/1/affiliations/1").to route_to("affiliations#destroy", id: "1", project_id: "1")
     end
   end
 end
