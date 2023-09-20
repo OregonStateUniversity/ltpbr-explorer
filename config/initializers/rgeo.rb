5967fbb29204396fa278438d6d0eef1b76067ad8
Yong Bakos <ybakos@humanoriented.com>
2023-09-20 16:47:00 -0700

    Config: Update geos factory
    rgeo_factory_generator and related methods were removed in version 4.0, since
    column types are no longer tied to their database column in ActiveRecord 4.2.
    
    See https://github.com/rgeo/rgeo/issues/103
    See https://github.com/rgeo/rgeo-activerecord/blob/master/README.md#factory-generator-changes
    
    References #286

--- config/initializers/rgeo.rb
+++ config/initializers/rgeo.rb
@@ -1,7 +1,7 @@
 # http://zackperdue.com/tutorials/installing-postgres-postgis-for-ruby-on-rails-and-heroku
 RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
   # By default, use the GEOS implementation for spatial columns.
-  config.default = RGeo::Geos.factory_generator
+  config.default = RGeo::Geos.factory
 
   # But use a geographic implementation for point columns.
   config.register(RGeo::Geographic.spherical_factory(srid: 4326), geo_type: "point")
