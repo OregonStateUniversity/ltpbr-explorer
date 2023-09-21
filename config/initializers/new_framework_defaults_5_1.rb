f26f8c7d87da05c21d9afad68fc130c55071cac5
Yong Bakos <ybakos@humanoriented.com>
2023-09-20 17:00:46 -0700

    Config: Load 7.0 framework defaults, delete old defaults
    References #286

--- config/initializers/new_framework_defaults_5_1.rb
+++
@@ -1,8 +0,0 @@
-# From 5.1.4 -> 5.1.5 -> 5.1.6
-
-# Make `form_with` generate non-remote forms.
-Rails.application.config.action_view.form_with_generates_remote_forms = true
-
-# Unknown asset fallback will return the path passed in when the given
-# asset is not present in the asset pipeline.
-Rails.application.config.assets.unknown_asset_fallback = false
