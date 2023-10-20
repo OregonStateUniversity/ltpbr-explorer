ab947bae2d4ce079f6926b1d3dcbde833363726e
Yong Bakos <ybakos@humanoriented.com>
2023-10-20 15:56:39 -0700

    Routes: Re-order and alphabetize.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
-  resources :organizations
   root 'static#home'
   get 'about', to: 'static#about'
   get 'projects-map', to: 'static#projects_map'
+
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :users, only: :show
-  resources :states
+
+  resources :organizations
   resources :projects do
     resources :affiliations
   end
+  resources :states
+  resources :users, only: :show
+
 end
