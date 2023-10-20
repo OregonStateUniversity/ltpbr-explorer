1ae0e116f1da003075a10277c177dc69d9f59a00
Yong Bakos <ybakos@humanoriented.com>
2023-10-20 15:55:48 -0700

    Specs: Delete worthless specs

--- spec/routing/static_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe StaticController, type: :routing do
-  describe 'routes static pages' do
-    it 'routes to #home' do
-      expect(get: '/').to route_to(controller: 'static', action: 'home')
-    end
-  end
-end
