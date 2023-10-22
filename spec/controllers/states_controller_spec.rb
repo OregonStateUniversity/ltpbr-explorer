9f077536a0ae4fa9823f682688d39735b4366c3c
Yong Bakos <ybakos@humanoriented.com>
2023-10-22 00:22:27 -0700

    StatesController: Stub controller test
    There is no authentication here, so no need to test access.
    
    References #312

---
+++ spec/controllers/states_controller_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe StatesController, type: :controller do
+
+  context 'As any user' do
+    describe 'GET #index' do
+      # Pass      
+    end
+
+    describe 'GET #show' do
+      # Pass
+    end
+  end
+
+end
