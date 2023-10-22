ad561fadc7c3eb735e350a6dacf20daa608a6cea
Yong Bakos <ybakos@humanoriented.com>
2023-10-22 00:01:12 -0700

    StatesController: Fix method name
    Add bang to accumulate method name.
    
    See 13379f60d8cfa4def44df144acf69fcfbf08b773
    
    References #312

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -11,9 +11,9 @@ class StatesController < ApplicationController
     @projects = @state.projects
     gon.state_name = @state.name
     gon.rabl :template => 'app/views/shared/projects.rabl'
-    @chart_project_count = @projects.group_by_day(:implementation_date, format: "%d %b %Y").count.accumulate
-    @chart_structure_count = @projects.group_by_day(:implementation_date).sum(:number_of_structures).accumulate
-    @chart_total_length = @projects.group_by_day(:implementation_date).sum(:length).accumulate.transform_values! { |v| v / 1000.0 }
+    @chart_project_count = @projects.group_by_day(:implementation_date, format: "%d %b %Y").count.accumulate!
+    @chart_structure_count = @projects.group_by_day(:implementation_date).sum(:number_of_structures).accumulate!
+    @chart_total_length = @projects.group_by_day(:implementation_date).sum(:length).accumulate!.transform_values! { |v| v / 1000.0 }
   rescue ActiveRecord::RecordNotFound
     redirect_to states_path, alert: 'That state does not exist.'
   end
