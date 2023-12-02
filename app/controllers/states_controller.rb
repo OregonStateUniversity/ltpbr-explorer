50fc11dbce8650b56c8e8a264ffd9ff55825fd62
Yong Bakos <ybakos@humanoriented.com>
2023-12-02 08:40:07 -0800

    States: order by project count
    References #365

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -3,7 +3,7 @@ require 'hash'
 class StatesController < ApplicationController
 
   def index
-    @states = State.with_projects.without_geom.order(:name)
+    @states = State.with_projects.without_geom.order(:projects_count)
   end
 
   def show
